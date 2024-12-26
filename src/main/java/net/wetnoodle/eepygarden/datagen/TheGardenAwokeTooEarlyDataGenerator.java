ec8379af6759ad5bc459505adbe826d448111eea
Wet Noodle <kluski42@gmail.com>
2024-12-25 21:58:58 -0500

    Moved datagen

--- src/main/java/net/wetnoodle/eepygarden/datagen/TheGardenAwokeTooEarlyDataGenerator.java
+++ src/main/java/net/wetnoodle/eepygarden/datagen/TheGardenAwokeTooEarlyDataGenerator.java
@@ -1,4 +1,4 @@
-package net.wetnoodle.eepygarden;
+package net.wetnoodle.eepygarden.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
